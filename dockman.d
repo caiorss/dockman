e4ee0e2007b970d6194aa96c820207c7b720b388
CaioRS <caiorss.rodrigues@gmail.com>
2020-05-06 20:31:16 -0300

    Refactor function docker_shell() - bundle all parameters in a single struct.

--- dockman.d
+++ dockman.d
@@ -7,6 +7,38 @@ import std.conv;
 import std.algorithm;
 import std.array;
 
+enum OptionFlags {
+        // (bit 0) Enable verbose - Show docker commands 
+        verbose = 1 << 0,
+        // (bit 1) Mount user $HOME directory to /uhome directory in container 
+        home    = 1 << 1,
+        // (bit 2) Enable X11 forwarding for displaying GUI applications 
+        x11     = 1 << 2,
+        // (bit 3) Ennable ptrace capability 
+        ptrace  = 1 << 3,
+}
+
+bool get_bit_from_flags(int flags, int bit)
+{
+        return ((flags >> bit) & 1) == 1;
+}
+
+struct DockerOptions {
+        string   docker_image = null;
+        int      flags      = 0;   
+        bool     verbose    = false;
+        bool     home       = false;
+        bool     x11        = false;     
+        bool     detach     = false;        
+        string   entrypoint = null;
+        string   command    = null;
+        string   user       = null;
+        string   name       = null;
+        string   workdir    = null; 
+        string[] volumes    = null;
+        string[] ports      = null;
+}
+
 /** Main function is the entry-point of a D-program */
 void main(string[] args) 
 {               
@@ -21,7 +53,9 @@ void main(string[] args)
         string[] volumes  = null;
         string[] ports    = null;
 
-        bool dont_remove_container = false;
+        DockerOptions dopts;
+
+        // bool dont_remove_container = false;
 
         opt.GetoptResult opt_result;
 
@@ -32,16 +66,16 @@ void main(string[] args)
         try {
                 opt_result = opt.getopt(
                                 args
-                                ,"verbose",      "Log docker commands for debugging.", &verbose
+                                ,"verbose",      "Log docker commands for debugging.", &dopts.verbose
                                 ,"w|workdir",    "Working directory, default current directory of host.", &workdir
-                                ,"n|name",       "Human-readable name for container." ,&name
-                                ,"c|command",    "Command to be executed by image entrypoint", &command
-                                ,"e|entrypoint", "Alternative entrypoint.", &entrypoint
-                                ,"x|x11",        "Enable X11 GUI graphical user interface", &x11
-                                ,"u|user",       "Alternative user.", &user
-                                ,"m|home",       "Mount $HOME directory to /uhome dir. in container." ,&home                                
-                                ,"v|volume",     "Volume to be mounted.", &volumes
-                                ,"p|port",       "Host port to be mapped to container.", &ports
+                                ,"n|name",       "Human-readable name for container." ,&dopts.name
+                                ,"c|command",    "Command to be executed by image entrypoint", &dopts.command
+                                ,"e|entrypoint", "Alternative entrypoint.", &dopts.entrypoint
+                                ,"x|x11",        "Enable X11 GUI graphical user interface", &dopts.x11
+                                ,"u|user",       "Alternative user.", &dopts.user
+                                ,"m|home",       "Mount $HOME directory to /uhome dir. in container." ,&dopts.home
+                                ,"v|volume",     "Volume to be mounted.", &dopts.volumes
+                                ,"p|port",       "Host port to be mapped to container.", &dopts.ports
                                 /* ,"r|drmc",       "Dont remove container (default false)", &dont_remove_container */
                         );
 
@@ -74,6 +108,9 @@ void main(string[] args)
                         return;
                 }
 
+                dopts.docker_image = args[2];
+                dopts.command = join(cmd_args, " ");
+
                 /+ auto arglist = args.filter!((string x) => !startsWith(x, "-"));
                 io.writeln(" arglist = ", arglist);
                  +/       
@@ -83,14 +120,11 @@ void main(string[] args)
                 }
 
                 if(args[1] == "shell")  
-                        docker_shell( args[2], workdir, entrypoint, command = join(cmd_args, " "), user, name
-                                        , home, x11, verbose, false, false, volumes, ports);                      
+                        docker_shell( &dopts );                      
 
                 if(args[1] == "daemon"){ 
                         bool flag_dont_remove_container = name != null;
-                        docker_shell( args[2], workdir, entrypoint, command, user, name
-                                        , home, x11, verbose, true
-                                        , flag_dont_remove_container, volumes, ports);        
+                        docker_shell( &dopts );        
                 }
                 
                 if(args[1] == "build")
@@ -123,85 +157,73 @@ string[] parse_remaining_args(string[] args)
         return cp;
 }
 
-void docker_shell(  string docker_image
-                      , string workdir      = null 
-                      , string entrypoint   = null 
-                      , string command      = null 
-                      , string user         = null 
-                      , string name         = null 
-                      , bool enable_home    = false
-                      , bool enable_x11     = false
-                      , bool enable_verbose = false 
-                      , bool enable_daemon  = false
-                      , bool dont_remove_container = false
-                      , string[] volumes    = []
-                      , string[] ports      = []
-                     )
+void docker_shell(DockerOptions* dpt)
 {
-        auto docker_args = ["docker", "run", "-it"]; 
+        auto docker_args = ["docker", "run", "-it", "--rm"]; 
 
         // Get current directory 
         string cwd = sf.getcwd();
         // Get home directory 
         string home = sp.environment.get("HOME");
 
-        if( !dont_remove_container ) docker_args ~= ["--rm"];
+        //if( !dont_remove_container ) docker_args ~= ["--rm"];
 
-        if(enable_daemon) docker_args ~= ["--detach"];
+        
+        // if(dpt.enable_daemon) docker_args ~= ["--detach"];
 
-        if(user != null){
-                if(enable_verbose) io.writefln(" [TRACE] User changed to: '%s'", user);
-                docker_args ~= ["--user", user];
+        if(dpt.user != null){
+                // if(enable_verbose) io.writefln(" [TRACE] User changed to: '%s'", user);
+                docker_args ~= ["--user", dpt.user];
         }        
 
-        if(name != null){
-                docker_args ~= ["--name", name];
+        if(dpt.name != null){
+                docker_args ~= ["--name", dpt.name];
         }
 
         // Directory to be mounted to '/work' dir in container. 
-        string wdir = workdir != null ? workdir : cwd;
+        string wdir = dpt.workdir != null ? dpt.workdir : cwd;
 
-        if(enable_verbose) io.writefln(" [TRACE] Mount %s to /work ", wdir);
+        if(dpt.verbose) io.writefln(" [TRACE] Mount %s to /work ", wdir);
         // Concatenate list 
         docker_args ~= ["-v", wdir ~ ":/work"];
         // Set /work as current directory in container 
         docker_args ~= ["-w", "/work"];
                 
-        if(enable_home) {
-                if(enable_verbose) io.writefln(" [TRACE] Mount %s to /uhome ", home);
+        if(dpt.home) {
+                if(dpt.verbose) io.writefln(" [TRACE] Mount %s to /uhome ", home);
                 docker_args ~= ["-v", home ~ ":/uhome"];
         }
 
-        if(enable_x11) {
-                if(enable_verbose) io.writefln(" [TRACE] Enable X11 - graphical user interfaces ");
+        if(dpt.x11) {
+                if(dpt.verbose) io.writefln(" [TRACE] Enable X11 - graphical user interfaces ");
                 sp.executeShell("xhost +local:docker");
                 docker_args ~= ["-e", "DISPLAY"];
                 docker_args ~= ["-v", "/tmp/.X11-unix:/tmp/.X11-unix"];
                 docker_args ~= ["-v", home ~ "/.Xauthority:/root/.Xauthority"];
         }
 
-        if(entrypoint != null) {
-                if(enable_verbose) io.writefln(" [TRACE] Entrypoint changed to: %s", entrypoint);
-                docker_args ~= ["--entrypoint=" ~ entrypoint];
+        if(dpt.entrypoint != null) {
+                if(dpt.verbose) io.writefln(" [TRACE] Entrypoint changed to: %s", dpt.entrypoint);
+                docker_args ~= ["--entrypoint=" ~ dpt.entrypoint];
         }
 
         
-        foreach (v; volumes)
+        foreach (v; dpt.volumes)
         {                        
                 //assert(v.split(":").length != 2);
                 docker_args ~= ["-v", v];
         }
 
-        foreach (p; ports)
+        foreach (p; dpt.ports)
         {
                 docker_args ~= ["-p", p];
         }
         
-        docker_args ~= [ docker_image ];
+        docker_args ~= [ dpt.docker_image ];
 
-        if(command != null) docker_args ~= command.split(" ");
+        if(dpt.command != null) docker_args ~= dpt.command.split(" ");
 
-        if(enable_verbose) io.writeln(" Docker command run: \n ", docker_args);
+        if(dpt.verbose) io.writeln(" Docker command run: \n ", docker_args);
 
         auto d1 = sp.spawnProcess(docker_args);            
         sp.wait(d1);
